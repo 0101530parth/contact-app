a12c18d9257490bed59d9b44ec89ae568019a68f
Skywalkers-24 <72136648+Skywalkers-24@users.noreply.github.com>
2023-03-09 15:22:07 +0530

    Update ContactCard.js

--- src/components/ContactCard.js
+++ src/components/ContactCard.js
@@ -1,28 +1,25 @@
-import React from 'react';
-import {Link} from 'react-router-dom';
-import user from "./user.png"
-const ContactCard = (props) => {
-  console.log(props.contacts,"ContactCard");
-    const { id, name, email } = props.contacts;
-    return (
-      <div className="item">
-        <img className="ui avatar image" src={user} alt="user" />
-        <div className="content">
-          <Link
-            to={{ pathname: `/contact/${id}`, state:{ contact: props.contacts } }}
-          >
-            <div className="header">{name}</div>
-            <div>{email}</div>
-          </Link>
-        </div>
-        <i
-          className="trash alternate outline icon"
-          style={{ color: "red", marginTop: "7px" }}
-          onClick={() => props.clickHander(id)}
-        ></i>
+import React from "react";
+import { Link } from "react-router-dom";
+import user from "./user.png";
+const ContactCard = ({ contact, clickHander }) => {
+  const { id, name, email } = contact;
+  
+  return (
+    <div className="item">
+      <img className="ui avatar image" src={user} alt="user" />
+      <div className="content">
+        <Link to={{ pathname: `/contact/${id}` }} state={{ contact: contact }}>
+          <div className="header">{name}</div>
+          <div>{email}</div>
+        </Link>
       </div>
-    );
-  };
-  
-  export default ContactCard;
-  
+      <i
+        className="trash alternate outline icon"
+        style={{ color: "red", marginTop: "7px" }}
+        onClick={() => clickHander(id)}
+      ></i>
+    </div>
+  );
+};
+
+export default ContactCard;
